20a682073b3e2fbf5b42ab25c550f39124f6099e
Lagreze <lagrezd@gmail.com>
2017-07-17 18:57:20 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+      <v-toolbar>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>Meetup Clone</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <!-- v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon -->
+          <v-toolbar-items>
+              <v-btn flat>
+                  <v-icon>supervisor_account</v-icon>
+                  Voir les Meetups
+              </v-btn>
+          </v-toolbar-items>
+      </v-toolbar>
+    <main>
+        Tester mon meetup
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
