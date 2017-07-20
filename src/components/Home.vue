78e067786869a7c44bcad9b5ccd152e1485f45b6
damien <lagrezd@gmail.com>
2017-07-21 00:52:48 +0200

    Feature - slideshow meetups page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>
-   <p>The home page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large to="/meetups" class="info">Rechercher un Meetup</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large to="/meetup/new" class="info">Organiser un Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Rejoindre votre Meetup magique !</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -9,10 +35,30 @@ export default {
   name: 'home',
   data () {
     return {
+      meetups: [
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg',
+          id: '',
+          title: 'Meetup in New York'
+        },
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/8a/Paris_vue_d%27ensemble_tour_Eiffel.jpg',
+          id: '',
+          title: 'Meetup in Paris'
+        }
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color:white;
+    font-size: 2em;
+    padding: 30px;
+  }
 </style>
